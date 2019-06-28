c9c465bb7fd145cb906bfe34fc530e2c8847cbcb
Kuba Krafka <jakub.krafka@datlab.eu>
2019-06-28 10:39:08 +0200

    get value tests and simplify functionality

--- contracting_process/processor.py
+++ contracting_process/processor.py
@@ -16,24 +16,48 @@ def do_work(item):
     return
 
 
-def get_value(item, path):
+def get_value(item, path, simplify=False):
+    print(simplify)
+    # return whole item from root
+    if not path or path == [""]:
+        return item
 
+    # return the value for key in the item
     if type(path) is str:
         return item[path]
+
+    # last item in the path - return the key value
     if len(path) == 1:
         return item[path[0]]
 
+    # get new key identifying the new item
     key = path[0]
 
     if key in item:
+        # inner value is a dictionary { "key": {"aaa": "bbb"}}
+        # lets go deeper
         if type(item[key]) is dict:
-            return get_value(item[key], path[1:])
+            return get_value(item[key], path[1:], simplify)
+
+        # inner value is an aarray { "key" : [{"aaa":"bbb"}, {"ccc": "ddd"}]}
+        # iterate over the items and read the rest of the path from the
         if type(item[key]) is list:
             values = []
             for list_item in item[key]:
-                values.append(get_value(list_item, path[1]))
+                result = get_value(list_item, path[1:], simplify)
+
+                if type(result) is not list:
+                    values.append(result)
+                else:
+                    if simplify:
+                        values = values + result
+                    else:
+                        values.append(result)
+
             return values
 
+        # "primitive" value, return it
         return item[key]
 
+    # nothing found
     return None
--- contracting_process/tests/test_processor.py
+++ contracting_process/tests/test_processor.py
@@ -6,64 +6,39 @@ item = {
         "id": "0rw29R-005-2011-1",
         "items": [
             {
-                "id": "0rw29R005-2011197482",
                 "unit": {"name": "Unidad"},
                 "quantity": 4.0,
-                "description": "Llanta para vehiculo # 235/75R15 radiales de 8 o 10 lonas",
-                "classification": {
-                    "id": "25171901",
-                    "scheme": "UNSPSC",
-                    "description": "Llantas y rines para automóviles"
-                }
-            }
+                "inner_list": [{"aaa": "bbb"}, {"aaa": "ccc"}]
+            },
+            {
+                "unit": {"name": "Unimom"},
+                "quantity": 5.0,
+                "inner_list": [{"aaa": "ddd"}, {"aaa": "eee"}]
+            },
         ],
-    },
-    "parties": [
-        {
-            "id": "7rBOL0",
-            "name": "Secretaria de Desarrollo Economico",
-            "roles": ["buyer"],
-            "address": {
-                "streetAddress": "Edificio San José,3 piso Boulevard KUWAIT "
-            },
-            "identifier": {"scheme": "HN-ONCAE-CE"},
-            "contactPoint": {
-                "url": "http://www.sic.gob.hn",
-                "email": "2235-3678",
-                "faxNumber": "2235-5037"
-            }
-        },
-        {
-            "id": "0rw29R-7rBOL0",
-            "name": "Unidad Central",
-            "roles": ["buyer"],
-            "address": {
-                "region": "Francisco Morazan",
-                "locality": "DISTRITO CENTRAL",
-                "streetAddress": "Edificio San José Boulevard KUWAIT 3 piso FENADUANAH"
-            },
-            "memberOf": [
-                {"id": "7rBOL0", "name": "Secretaria de Desarrollo Economico"}
-            ],
-            "identifier": {"scheme": "HN-ONCAE-CE"},
-            "contactPoint": {
-                "url": "http://www.sic.gob.hn",
-                "name": "Ranulfo Reyes Ramírez ",
-                "email": "sonalfa@sic.gob.hn, arojas@sic.gob.hn, rreyes@sic.gob.hn",
-                "faxNumber": "235-5037",
-                "telephone": "235-5037"
-            }
-        }
-    ],
-    "sources": [
-        {
-            "id": "honducompras-1",
-            "url": "http://h1.honducompras.gob.hn/",
-            "name": "HonduCompras 1.0 - Módulo de Difusión de Compras y Contrataciones"
-        }
-    ],
+    }
 }
 
 
-def test_get_value():
+def test_get_value_simple():
     assert get_value(item, ["id"]) == "123"
+    assert get_value(item, [""]) == item
+    assert get_value(item, ["tender"]) == item["tender"]
+    assert get_value(item, ["tender", "id"]) == "0rw29R-005-2011-1"
+    assert get_value(item, ["tender", "id"]) == item["tender"]["id"]
+
+
+def test_get_value_lists():
+    assert get_value(item, ["tender", "items", "quantity"]) == [4.0, 5.0]
+    assert get_value(item, ["tender", "items", "unit"]) == [{"name": "Unidad"}, {"name": "Unimom"}]
+    assert get_value(item, ["tender", "items", "unit", "name"]) == ["Unidad", "Unimom"]
+
+
+def test_join():
+    assert get_value(item, ["tender", "items", "inner_list", "aaa"]) == [["bbb", "ccc"], ["ddd", "eee"]]
+    assert get_value(item, ["tender", "items", "inner_list", "aaa"], simplify=True) == ["bbb", "ccc", "ddd", "eee"]
+
+    assert get_value(item, ["tender", "items", "inner_list"], simplify=True) == [
+        {"aaa": "bbb"}, {"aaa": "ccc"}, {"aaa": "ddd"}, {"aaa": "eee"}]
+    assert get_value(item, ["tender", "items", "inner_list"]) == [
+        [{"aaa": "bbb"}, {"aaa": "ccc"}], [{"aaa": "ddd"}, {"aaa": "eee"}]]
