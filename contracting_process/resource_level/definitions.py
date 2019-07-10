16af266359126922dec9ed671da25df08dc65d5d
Kuba Krafka <jakub.krafka@datlab.eu>
2019-07-10 17:25:50 +0200

    added check to definitions

--- contracting_process/resource_level/definitions.py
+++ contracting_process/resource_level/definitions.py
@@ -1,5 +1,7 @@
 from contracting_process.resource_level.consistent import number_of_tenderers
+from contracting_process.resource_level.reference import supplier_in_parties
 
 definitions = {
-    "consistent.number_of_tenderers": [number_of_tenderers.calculate]
+    "consistent.number_of_tenderers": [number_of_tenderers.calculate],
+    "reference.supplier_in_parties": [supplier_in_parties.calculate]
 }
