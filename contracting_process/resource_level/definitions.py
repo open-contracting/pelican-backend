47ed2d336dcdcf43e65bb553c482df005041c167
Kuba Krafka <jakub.krafka@datlab.eu>
2019-07-11 12:30:56 +0200

    fixed definitions

--- contracting_process/resource_level/definitions.py
+++ contracting_process/resource_level/definitions.py
@@ -4,6 +4,6 @@ from contracting_process.resource_level.reference import (supplier_in_parties,
 
 definitions = {
     "consistent.number_of_tenderers": [number_of_tenderers.calculate],
-    "reference.supplier_in_parties": [supplier_in_parties.calculate]
+    "reference.supplier_in_parties": [supplier_in_parties.calculate],
     "reference.tenderer_in_parties": [tenderer_in_parties.calculate]
 }
