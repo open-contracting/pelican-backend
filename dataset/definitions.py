027749a3b0835dfcfe46de0a504d60e2d2f740e6
Kuba Krafka <jakub.krafka@datlab.eu>
2019-07-14 20:15:19 +0200

    fxed resend from extractor

--- dataset/definitions.py
+++ dataset/definitions.py
@@ -1,7 +1,8 @@
-from dataset.distribution import main_procurement_category
+from dataset.distribution import contracts_value, main_procurement_category
 from dataset.unique import id
 
 definitions = {
     "distribution.main_procurement_category": main_procurement_category,
     "unique.id": id,
+    "distribution.contracts_value": contracts_value
 }
