209acdee7a28ea89fe36b5345817d6c618f8d982
Miroslav Březík <mirek.brezik@gmail.com>
2019-07-25 13:31:54 +0200

    tender.value.consistent check implemented; needs to be unit-tested

---
+++ contracting_process/resource_level/consistent/tender_value.py
@@ -0,0 +1,80 @@
+from tools.checks import get_empty_result_resource
+from tools.getter import get_values
+
+from currency_converter import CurrencyConverter
+from datetime import datetime
+
+
+version = 1.0
+
+def calculate(item):
+    result = get_empty_result_resource(version)
+
+    tender_value = get_values(item, 'tender.value')
+    planning_budget_amount = get_values(item, 'planning.budget.amount')
+
+    if not tender_value or not planning_budget_amount:
+        result['meta'] = {'reason': 'values are not set'}
+        return result
+
+    tender_value = tender_value[0]['value']
+    planning_budget_amount = planning_budget_amount[0]['value']
+
+    # missing fields
+    if 'amount' not in tender_value or 'currency' not in tender_value or \
+       'amount' not in planning_budget_amount or 'currency' not in planning_budget_amount:
+        
+        result['meta'] = {'reason': 'amount or currency is not set'}
+        return result
+
+    # None fields
+    if tender_value['amount'] is None or tender_value['currency'] is None or \
+       planning_budget_amount['amount'] is None or planning_budget_amount['currency'] is None:
+
+       result['meta'] = {'reason': 'amount or currency is null'}
+       return result
+
+    cc = CurrencyConverter("http://www.ecb.int/stats/eurofxref/eurofxref-hist.zip", fallback_on_wrong_date=True)
+    
+    # non-supported currencies
+    if tender_value['currency'] not in cc.currencies or planning_budget_amount['currency'] not in cc.currencies:
+        result['meta'] = {'reason': 'non-supported currency', 'tender.value': tender_value, 'planning.budget.amount': planning_budget_amount}
+        return result
+
+    # currency conversion if necessary
+    if tender_value['currency'] == planning_budget_amount['currency']:
+        tender_value_amount = tender_value['amount']
+        planning_budget_amount_amount = planning_budget_amount['amount']
+    else:
+        ref_date = datetime.strptime(item.get_values(item, 'date')[0]['value'][:9], '%Y-%m-%d').date()
+        tender_value_amount = cc.convert(tender_value['amount'], tender_value['currency'], 'USD', date=ref_date)
+        planning_budget_amount_amount = cc.convert(planning_budget_amount['amount'], planning_budget_amount['currency'],
+                                                   'USD', date=ref_date)
+    
+    # amount is equal to zero
+    if tender_value_amount == 0 or planning_budget_amount_amount == 0:
+        result['meta'] = {'reason': 'amount is equal to zero', 'tender.value': tender_value, 'planning.budget.amount': planning_budget_amount}
+        return result
+
+    # different signs
+    if (tender_value_amount > 0 and planning_budget_amount_amount < 0) or \
+       (tender_value_amount < 0 and planning_budget_amount_amount > 0):
+
+        result['meta'] = {'reason': 'amounts have different signs', 'tender.value': tender_value, 'planning.budget.amount': planning_budget_amount}
+        return result
+
+    ratio = abs(tender_value_amount - planning_budget_amount_amount) / abs(tender_value_amount)
+    passed = ratio <= 0.5
+
+    result['result'] = passed
+    result['meta'] = {'tender.value': tender_value, 'planning.budget.amount': planning_budget_amount}
+
+    return result
+
+
+
+
+    
+
+    
+
--- contracting_process/resource_level/definitions.py
+++ contracting_process/resource_level/definitions.py
@@ -1,7 +1,8 @@
 from contracting_process.resource_level.coherent import (
     procurement_method_vs_number_of_tenderers, tender_status)
 from contracting_process.resource_level.consistent import (buyer_roles,
-                                                           number_of_tenderers)
+                                                           number_of_tenderers,
+                                                           tender_value)
 from contracting_process.resource_level.reference import (
     buyer_in_parties, payee_in_parties, payer_in_parties,
     procuring_entity_in_parties, supplier_in_parties, tenderer_in_parties)
@@ -9,6 +10,7 @@ from contracting_process.resource_level.reference import (
 definitions = {
     "consistent.number_of_tenderers": [number_of_tenderers.calculate],
     "consistent.buyer_roles": [buyer_roles.calculate],
+    "consistent.tender_value": [tender_value.calculate],
     "reference.supplier_in_parties": [supplier_in_parties.calculate],
     "reference.tenderer_in_parties": [tenderer_in_parties.calculate],
     "reference.buyer_in_parties": [buyer_in_parties.calculate],
