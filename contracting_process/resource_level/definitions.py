5a35820e5f3abd12d3fc30786938f8827e14e80f
Miroslav Březík <mirek.brezik@gmail.com>
2019-07-25 11:23:25 +0200

    added period to definitions

--- contracting_process/resource_level/definitions.py
+++ contracting_process/resource_level/definitions.py
@@ -1,5 +1,5 @@
 from contracting_process.resource_level.coherent import (
-    procurement_method_vs_number_of_tenderers, tender_status)
+    procurement_method_vs_number_of_tenderers, tender_status, period)
 from contracting_process.resource_level.consistent import (buyer_roles,
                                                            number_of_tenderers)
 from contracting_process.resource_level.reference import (
@@ -17,4 +17,5 @@ definitions = {
     "reference.payee_in_parties": [payee_in_parties.calculate],
     "coherent.procurement_method_vs_number_of_tenderers": [procurement_method_vs_number_of_tenderers.calculate],
     "coherent.tender_status": [tender_status.calculate],
+    "coherent.period": [period.calculate]
 }
