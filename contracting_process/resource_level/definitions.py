ed6cf7dad7675862f7577e992064560af345ba1f
Miroslav Březík <mirek.brezik@gmail.com>
2019-08-05 12:59:27 +0200

    check finalized

--- contracting_process/resource_level/definitions.py
+++ contracting_process/resource_level/definitions.py
@@ -2,7 +2,9 @@ from contracting_process.resource_level.coherent import (
     dates, period, procurement_method_vs_number_of_tenderers, tender_status)
 from contracting_process.resource_level.consistent import (buyer_roles,
                                                            number_of_tenderers,
-                                                           tender_value)
+                                                           tender_value,
+                                                           contracts_value)
+                                                           
 from contracting_process.resource_level.reference import (
     buyer_in_parties, contract_in_awards, payee_in_parties, payer_in_parties,
     procuring_entity_in_parties, supplier_in_parties, tenderer_in_parties)
@@ -11,6 +13,7 @@ definitions = {
     "consistent.number_of_tenderers": [number_of_tenderers.calculate],
     "consistent.buyer_roles": [buyer_roles.calculate],
     "consistent.tender_value": [tender_value.calculate],
+    "consistent.contracts_value": [contracts_value.calculate],
     "reference.supplier_in_parties": [supplier_in_parties.calculate],
     "reference.tenderer_in_parties": [tenderer_in_parties.calculate],
     "reference.buyer_in_parties": [buyer_in_parties.calculate],
