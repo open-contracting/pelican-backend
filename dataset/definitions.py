364194bfb2d86f63822131cbbabb49a60c89c648
Miroslav Březík <mirek.brezik@gmail.com>
2019-08-05 15:37:41 +0200

    check implemented; started with testing

--- dataset/definitions.py
+++ dataset/definitions.py
@@ -1,5 +1,8 @@
 from dataset.distribution import (awards_value, contracts_value,
-                                  main_procurement_category, tender_value)
+                                  main_procurement_category, tender_value,
+                                  tender_value_repetition,
+                                  awards_value_repetition,
+                                  contracts_value_repetition)
 from dataset.unique import id
 
 definitions = {
@@ -8,4 +11,7 @@ definitions = {
     "distribution.contracts_value": contracts_value,
     "distribution.awards_value": awards_value,
     "distribution.tender_value": tender_value,
+    "distribution.tender_value_repetition": tender_value_repetition,
+    "distribution.awards_value_repetition": awards_value_repetition,
+    "distribution.contracts_value_repetition": contracts_value_repetition
 }
