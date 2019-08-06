a6847c40f895d46ed0133adc40e2423554971b18
Miroslav Březík <mirek.brezik@gmail.com>
2019-08-06 14:53:28 +0200

    check tested

--- dataset/definitions.py
+++ dataset/definitions.py
@@ -1,6 +1,7 @@
 from dataset.distribution import (awards_value, contracts_value,
                                   main_procurement_category, tender_value)
 from dataset.unique import id
+from dataset.misc import url_availability
 
 definitions = {
     "distribution.main_procurement_category": main_procurement_category,
@@ -8,4 +9,5 @@ definitions = {
     "distribution.contracts_value": contracts_value,
     "distribution.awards_value": awards_value,
     "distribution.tender_value": tender_value,
+    "misc.url_availability": url_availability
 }
