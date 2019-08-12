239ed81926c16fe218df7280f424e6a844165c01
Jaroslav <yaroslav.kolodka@gmail.com>
2019-08-12 17:37:41 +0200

    start

---
+++ contracting_process/resource_level/consistent/org_ref_name_consistent.py
@@ -0,0 +1,11 @@
+from tools.checks import get_empty_result_resource
+
+version = "1.0"
+
+
+def calculate(item):
+    result = get_empty_result_resource(version)
+    return result
+
+
+# def buyer_name_consistence():
