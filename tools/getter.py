e041e86a58ca9d1af2f2ee327050684ee28388da
Miroslav Březík <mirek.brezik@gmail.com>
2019-08-15 15:18:51 +0200

    added value only functionality for getter

--- tools/getter.py
+++ tools/getter.py
@@ -1,23 +1,32 @@
-def get_values(item, str_path):
+def get_values(item, str_path, value_only=False):
     # return whole item from root
     if not str_path or str_path == "":
-        return [{"path": str_path, "value": item}]
+        if value_only:
+            return [item]
+        else:
+            return [{"path": str_path, "value": item}]
 
     # return the value for key in the item
     if "." not in str_path and str_path in item:
         if type(item[str_path]) is list:
             values = []
             for index in range(len(item[str_path])):
-                values.append(
-                    {
-                        "path": "{}[{}]".format(str_path, index),
-                        "value": item[str_path][index]    
-                    }
-                )
+                if value_only:
+                    values.append(item[str_path][index])
+                else:
+                    values.append(
+                        {
+                            "path": "{}[{}]".format(str_path, index),
+                            "value": item[str_path][index]
+                        }
+                    )
 
             return values
         else:
-            return [{"path": str_path, "value": item[str_path]}]
+            if value_only:
+                return [item[str_path]]
+            else:
+                return [{"path": str_path, "value": item[str_path]}]
 
     # get new key identifying the new item
     path = str_path.split(".")
@@ -27,7 +36,7 @@ def get_values(item, str_path):
         # inner value is a dictionary { "key": {"aaa": "bbb"}}
         # lets go deeper
         if type(item[key]) is dict:
-            result = get_values(item[key], ".".join(path[1:]))
+            result = get_values(item[key], ".".join(path[1:]), value_only=value_only)
 
             if not result:
                 return []
@@ -39,7 +48,7 @@ def get_values(item, str_path):
                 values = result
 
             for list_item in values:
-                if list_item and "path" in list_item:
+                if not value_only and list_item and "path" in list_item:
                     list_item["path"] = "{}.{}".format(key, list_item["path"])
             return values
 
@@ -49,14 +58,18 @@ def get_values(item, str_path):
             index_counter = 0
             result = []
             for list_item in item[key]:
-                values = get_values(list_item, ".".join(path[1:]))
+                values = get_values(list_item, ".".join(path[1:]), value_only=value_only)
 
                 if values:
                     for list_item in values:
-                        if list_item and "path" in list_item:
-                            list_item["path"] = "{}[{}].{}".format(key, index_counter, list_item["path"])
+                        if value_only:
+                            if list_item is not None:
+                                result.append(list_item)
+                        else:
+                            if list_item and "path" in list_item:
+                                list_item["path"] = "{}[{}].{}".format(key, index_counter, list_item["path"])
 
-                            result.append(list_item)
+                                result.append(list_item)
 
                 index_counter = index_counter + 1
 
@@ -64,7 +77,10 @@ def get_values(item, str_path):
 
         # "primitive" value, return it
         if key in item:
-            return [{"path": key, "value": item[key]}]
+            if value_only:
+                return [item[key]]
+            else:
+                return [{"path": key, "value": item[key]}]
 
     # nothing found
     return []
