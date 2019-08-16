14b075dc1670e1512afc831e569cec5092cebdef
Miroslav Březík <mirek.brezik@gmail.com>
2019-08-16 13:50:42 +0200

    lowered memory usage

--- dataset/reference/related_process_identifier.py
+++ dataset/reference/related_process_identifier.py
@@ -1,4 +1,6 @@
 
+import random
+
 from tools.checks import get_empty_result_dataset
 from tools.getter import get_values
 
@@ -9,14 +11,32 @@ examples_cap = 100
 def add_item(scope, item, item_id):
     if not scope:
         scope = {
-            'original_ocid': set(),
-            'related_processes': []
+            'original_ocid': dict(),
+            'related_processes': dict(),
+            'meta': {
+                'total_processed': 0,
+                'total_passed': 0,
+                'total_failed': 0,
+                'passed_examples': [],
+                'failed_examples': []
+            }
         }
 
     ocid = item['ocid']
 
     if ocid:
-        scope['original_ocid'].add(ocid)
+        if ocid in scope['original_ocid']:
+            scope['original_ocid'][ocid]['found'] = True
+            for key in scope['original_ocid'][ocid]['pending_related_processes']:
+                scope = pick_examples(scope, key, True)
+                del scope['related_processes'][key]
+
+            scope['original_ocid'][ocid]['pending_related_processes'].clear()
+        else:
+            scope['original_ocid'][ocid] = {
+                'pending_related_processes': [],
+                'found': True
+            }
 
     related_processes = []
     related_processes.extend(get_values(item, 'relatedProcesses'))
@@ -30,13 +50,25 @@ def add_item(scope, item, item_id):
         if 'identifier' not in related_process['value'] or related_process['value']['identifier'] is None:
             continue
 
-        scope['related_processes'].append(
-            {
-                'ocid': ocid,
-                'related_ocid': related_process['value']['identifier'],
-                'related_path': related_process['path']
+        key = (ocid, related_process['value']['identifier'])
+        scope['related_processes'][key] = {
+            'ocid': ocid,
+            'related_ocid': related_process['value']['identifier'],
+            'related_path': related_process['path']
+        }
+
+        if related_process['value']['identifier'] in scope['original_ocid']:
+            if scope['original_ocid'][related_process['value']['identifier']]['found']:
+                scope = pick_examples(scope, key, True)
+                del scope['related_processes'][key]
+            else:
+                scope['original_ocid'][related_process['value']['identifier']]['pending_related_processes'].append(key)
+
+        else:
+            scope['original_ocid'][related_process['value']['identifier']] = {
+                'pending_related_processes': [key],
+                'found': False
             }
-        )
 
     return scope
 
@@ -44,45 +76,46 @@ def add_item(scope, item, item_id):
 def get_result(scope):
     result = get_empty_result_dataset(version)
 
-    application_count = 0
-    pass_count = 0
-    passed_examples = []
-    failed_examples = []
+    for key in scope['related_processes']:
+        scope = pick_examples(scope, key, False)
 
-    for related_process in scope['related_processes']:
-        passed = related_process['related_ocid'] in scope['original_ocid']
-
-        if passed and len(passed_examples) < examples_cap:
-            passed_examples.append(
-                {
-                    'related_process': related_process,
-                    'result': passed
-                }
-            )
-        elif not passed and len(failed_examples) < examples_cap:
-            failed_examples.append(
-                {
-                    'related_process': related_process,
-                    'result': passed
-                }
-            )
-
-        application_count += 1
-        pass_count = pass_count + 1 if passed else pass_count
-
-    if application_count == 0:
+    if scope['meta']['total_processed'] == 0:
         result['meta'] = {
             'reason': 'there are no pairs of related processes with check-specific properties'
         }
     else:
-        result['result'] = application_count == pass_count
-        result['value'] = 100 * (pass_count / application_count)
-        result['meta'] = {
-            'total_processed': application_count,
-            'total_passed': pass_count,
-            'total_failed': application_count - pass_count,
-            'passed_examples': passed_examples,
-            'failed_examples': failed_examples
-        }
+        result['result'] = scope['meta']['total_passed'] == scope['meta']['total_processed']
+        result['value'] = 100 * (scope['meta']['total_passed'] / scope['meta']['total_processed'])
+        result['meta'] = scope['meta']
 
     return result
+
+
+def pick_examples(scope, related_process_key, result):
+    example = {
+        'related_process': scope['related_processes'][related_process_key],
+        'result': result
+    }
+
+    if result:
+        if scope['meta']['total_passed'] < examples_cap:
+            scope['meta']['passed_examples'].append(example)
+        else:
+            r = random.randint(0, scope['meta']['total_passed'])
+            if r < examples_cap:
+                scope['meta']['passed_examples'][r] = example
+
+        scope['meta']['total_passed'] += 1
+    else:
+        if scope['meta']['total_failed'] < examples_cap:
+            scope['meta']['failed_examples'].append(example)
+        else:
+            r = random.randint(0, scope['meta']['total_failed'])
+            if r < examples_cap:
+                scope['meta']['failed_examples'][r] = example
+
+        scope['meta']['total_failed'] += 1
+
+    scope['meta']['total_processed'] += 1
+
+    return scope
