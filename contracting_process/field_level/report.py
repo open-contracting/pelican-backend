7a9313a96e244ece6427f415a608aee760a927cb
Miroslav Březík <mirek.brezik@gmail.com>
2019-09-05 11:38:58 +0200

    changed field_level_checks report

--- contracting_process/field_level/report.py
+++ contracting_process/field_level/report.py
@@ -7,8 +7,6 @@ from tools.db import get_cursor
 from tools.helpers import ReservoirSampler
 from contracting_process.field_level.definitions import coverage_checks, definitions
 
-check_examples_cap = 10
-
 
 def create(dataset_id):
     cursor = get_cursor()
@@ -17,6 +15,7 @@ def create(dataset_id):
 
     for path, quality_checks in definitions.items():
         report[path] = {
+            'examples_filled': False,
             'coverage': {
                 'checks': {},
                 'passed_count': 0,
