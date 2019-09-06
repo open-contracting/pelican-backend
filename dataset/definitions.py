9bbe1e11ffe64aa464520ba493aa6ec5c4c019e5
Jaroslav <yaroslav.kolodka@gmail.com>
2019-09-06 14:37:17 +0200

    changes in definitions.py

--- dataset/definitions.py
+++ dataset/definitions.py
@@ -4,7 +4,16 @@ from dataset.distribution import (
     tender_status,
     value_repetition,
     value,
-    tender_procurement_method
+    tender_procurement_method,
+    tender_award_criteria,
+    tender_submission_method,
+    awards_status,
+    contracts_status,
+    milestone_status,
+    milestone_type,
+    document_document_type,
+    value_currency,
+    related_process_relation
 )
 
 from dataset.misc import (
@@ -35,6 +44,15 @@ definitions = {
     "distribution.contracts_value_repetition": value_repetition.ModuleType("contracts"),
     "distribution.tender_procurement_method": tender_procurement_method.TenderProcurementMethodPathClass(),
     "distribution.tender_status": tender_status.TenderStatusPathClass(),
+    "distribution.tender_award_criteria": tender_award_criteria.TenderAwardCriteriaPathClass()
+    "distribution.tender_submission_method": tender_submission_method.TenderSubmissionMethodPathClass(),
+    "distribution.awards_status": awards_status.AwardsStatusPathClass(),
+    "distribution.contracts_status": contracts_status.ContractsStatusPathClass(),
+    "distribution.milestone_status": milestone_status.MilestoneStatusPathClass(),
+    "distribution.milestone_type": milestone_type.MilestoneTypePathClass(),
+    "distribution.document_document_type": document_document_type.DocumentDocumentTypePathClass(),
+    "distribution.value_currency": value_currency.ValueCurrencyPathClass(),
+    "distribution.related_process_relation": related_process_relation.RelatedProcessRelationPathClass(),
     "misc.url_availability": url_availability,
     "consistent.related_process_title": related_process_title,
     "reference.related_process_identifier": related_process_identifier
