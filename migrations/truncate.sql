ed9d0b90b401c292236298352f2b9c127e502525
Kuba Krafka <jakub@krafka.net>
2019-09-29 10:17:05 +0200

    pudated truncate script

--- migrations/truncate.sql
+++ migrations/truncate.sql
@@ -6,5 +6,8 @@ TRUNCATE TABLE progress_monitor_dataset;
 TRUNCATE TABLE progress_monitor_item;
 TRUNCATE TABLE data_item;
 TRUNCATE TABLE field_level_check;
+TRUNCATE TABLE field_level_check_examples;
 TRUNCATE TABLE resource_level_check;
+TRUNCATE TABLE resource_level_check_examples;
 TRUNCATE TABLE dataset_level_check;
+TRUNCATE TABLE time_variance_level_check;
