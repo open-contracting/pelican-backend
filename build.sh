d798f4b8d09f9c36c66261aa85e46828ca49fa8e
Kuba Krafka <jakub@krafka.net>
2020-05-15 11:13:14 +0200

    updated build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t dqt:production .
-$(aws ecr get-login --no-include-email --region eu-central-1 --profile datlab)
+aws ecr get-login-password --region eu-central-1 --profile datlab | docker login --username AWS --password-stdin 170654793963.dkr.ecr.eu-central-1.amazonaws.com
 docker tag dqt:production 170654793963.dkr.ecr.eu-central-1.amazonaws.com/dqt:production
 docker push 170654793963.dkr.ecr.eu-central-1.amazonaws.com/dqt:production
