059a5ceb0afb7d43a5c33ee34b4f0c145daaf4d5
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-05-06 15:37:04 -0400

    ci: Add shellcheck and shfmt

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 docker build -t dqt:production .
 aws ecr get-login-password --region eu-central-1 --profile datlab | docker login --username AWS --password-stdin 170654793963.dkr.ecr.eu-central-1.amazonaws.com
 docker tag dqt:production 170654793963.dkr.ecr.eu-central-1.amazonaws.com/dqt:production
