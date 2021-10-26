b3daaf910e4780de46a6efd31ba252d54ccfa1c3
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-10-25 21:40:33 -0400

    docs: Add comments to settings. Fix application_name.

--- settings/settings.py
+++ settings/settings.py
@@ -11,12 +11,16 @@ class CustomLogLevels:
 
 # Logging
 
+# The log level of the stream handler.
 LOG_LEVEL = getattr(logging, os.getenv("LOG_LEVEL", "WARNING"))
+# The file path of the file handler. The file handler's level is always DEBUG.
 LOG_FILENAME = os.getenv("LOG_FILENAME")
 
 # Batch sizes
 
+# Extractors collect this number of items before publishing a message.
 EXTRACTOR_MAX_BATCH_SIZE = int(os.getenv("EXTRACTOR_MAX_BATCH_SIZE", 100))
+# Do not import compiled releases whose size is larger than this number of bytes.
 KINGFISHER_PROCESS_MAX_SIZE = int(os.getenv("KINGFISHER_PROCESS_MAX_SIZE", 300000))
 
 # Local services
@@ -24,7 +28,7 @@ KINGFISHER_PROCESS_MAX_SIZE = int(os.getenv("KINGFISHER_PROCESS_MAX_SIZE", 30000
 # To set the search path, use, for example: postgresql:///pelican_backend?options=-csearch_path%3Dproduction,public
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql:///pelican_backend?application_name=pelican_backend")
 KINGFISHER_PROCESS_DATABASE_URL = os.getenv(
-    "KINGFISHER_PROCESS_DATABASE_URL", "postgresql:///kingfisher_process?application_name=kingfisher_process"
+    "KINGFISHER_PROCESS_DATABASE_URL", "postgresql:///kingfisher_process?application_name=pelican_backend"
 )
 
 RABBIT_URL = os.getenv("RABBIT_URL", "amqp://localhost")
@@ -32,6 +36,7 @@ RABBIT_EXCHANGE_NAME = os.getenv("RABBIT_EXCHANGE_NAME", "pelican_development")
 
 # Third-party services
 
+# Do not set in development, to skip updates to exchange rates, and to save quota.
 FIXER_IO_API_KEY = os.getenv("FIXER_IO_API_KEY")
 
 SENTRY_DSN = os.getenv("SENTRY_DSN")
