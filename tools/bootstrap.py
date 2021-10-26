1f29c682ba66d19cc69919da24360e126787ecf3
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-10-26 16:14:36 -0400

    test: Speed up tests by restoring exchange_rates_test.py (removed in b5de512f3e57a09dfbf55e68eb6f12a29ff4ef6d)

--- tools/bootstrap.py
+++ tools/bootstrap.py
@@ -1,9 +1,16 @@
-import tools.exchange_rates_db
+import os
+
 from tools.currency_converter import import_data
 from tools.logging_helper import init_logger
+
+# https://docs.pytest.org/en/latest/example/simple.html#pytest-current-test-environment-variable
+if "PYTEST_CURRENT_TEST" in os.environ:
+    import tools.exchange_rates_db as exchange_rates
+else:
+    import tools.exchange_rates_test as exchange_rates
 
 
 def bootstrap(logger_name):
     init_logger(logger_name)
 
-    import_data(tools.exchange_rates_db.load())
+    import_data(exchange_rates.load())
