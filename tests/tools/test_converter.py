24abba118ee808247bd259486b2ca572fd0cbc28
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-10-27 13:51:08 -0400

    test: Rename classes for clarity about what condition they are testing

--- tests/tools/test_converter.py
+++ tests/tools/test_converter.py
@@ -8,35 +8,35 @@ import tools.currency_converter as cc
 #################
 
 
-class mock_settings_test_interpolation1:
+class interpolation_none:
     CURRENCY_CONVERTER_INTERPOLATION = None
     CURRENCY_CONVERTER_EXTRAPOLATION = None
     CURRENCY_CONVERTER_INTERPOLATION_MAX_DAYS_FALLBACK = -1
     CURRENCY_CONVERTER_EXTRAPOLATION_MAX_DAYS_FALLBACK = -1
 
 
-class mock_settings_test_interpolation2:
+class interpolation_closest:
     CURRENCY_CONVERTER_INTERPOLATION = "closest"
     CURRENCY_CONVERTER_EXTRAPOLATION = None
     CURRENCY_CONVERTER_INTERPOLATION_MAX_DAYS_FALLBACK = -1
     CURRENCY_CONVERTER_EXTRAPOLATION_MAX_DAYS_FALLBACK = -1
 
 
-class mock_settings_test_interpolation3:
+class interpolation_closest_with_max_days:
     CURRENCY_CONVERTER_INTERPOLATION = "closest"
     CURRENCY_CONVERTER_EXTRAPOLATION = None
     CURRENCY_CONVERTER_INTERPOLATION_MAX_DAYS_FALLBACK = 1
     CURRENCY_CONVERTER_EXTRAPOLATION_MAX_DAYS_FALLBACK = -1
 
 
-class mock_settings_test_interpolation4:
+class interpolation_linear:
     CURRENCY_CONVERTER_INTERPOLATION = "linear"
     CURRENCY_CONVERTER_EXTRAPOLATION = None
     CURRENCY_CONVERTER_INTERPOLATION_MAX_DAYS_FALLBACK = -1
     CURRENCY_CONVERTER_EXTRAPOLATION_MAX_DAYS_FALLBACK = -1
 
 
-class mock_settings_test_interpolation5:
+class interpolation_linear_with_max_days:
     CURRENCY_CONVERTER_INTERPOLATION = "linear"
     CURRENCY_CONVERTER_EXTRAPOLATION = None
     CURRENCY_CONVERTER_INTERPOLATION_MAX_DAYS_FALLBACK = 1
@@ -49,7 +49,7 @@ item_test_interpolation2 = [(date(2019, 1, 1), {"CZK": 1, "HNL": 1}), (date(2019
 
 
 def test_interpolation():
-    with patch.object(cc, "settings", new=mock_settings_test_interpolation1):
+    with patch.object(cc, "settings", new=interpolation_none):
         cc.import_data(item_test_interpolation1)
 
         assert cc.convert(1, "CZK", "HNL", date(2018, 12, 31)) is None
@@ -59,7 +59,7 @@ def test_interpolation():
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 4)) == 4
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 5)) is None
 
-    with patch.object(cc, "settings", new=mock_settings_test_interpolation2):
+    with patch.object(cc, "settings", new=interpolation_closest):
         cc.import_data(item_test_interpolation1)
 
         assert cc.convert(1, "CZK", "HNL", date(2018, 12, 31)) is None
@@ -69,7 +69,7 @@ def test_interpolation():
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 4)) == 4
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 5)) is None
 
-    with patch.object(cc, "settings", new=mock_settings_test_interpolation3):
+    with patch.object(cc, "settings", new=interpolation_closest_with_max_days):
         cc.import_data(item_test_interpolation2)
 
         assert cc.convert(1, "CZK", "HNL", date(2018, 12, 31)) is None
@@ -81,7 +81,7 @@ def test_interpolation():
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 6)) == 6
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 7)) is None
 
-    with patch.object(cc, "settings", new=mock_settings_test_interpolation4):
+    with patch.object(cc, "settings", new=interpolation_linear):
         cc.import_data(item_test_interpolation1)
 
         assert cc.convert(1, "CZK", "HNL", date(2018, 12, 31)) is None
@@ -91,7 +91,7 @@ def test_interpolation():
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 4)) == 4
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 5)) is None
 
-    with patch.object(cc, "settings", new=mock_settings_test_interpolation5):
+    with patch.object(cc, "settings", new=interpolation_linear_with_max_days):
         cc.import_data(item_test_interpolation2)
 
         assert cc.convert(1, "CZK", "HNL", date(2018, 12, 31)) is None
@@ -109,14 +109,14 @@ def test_interpolation():
 #################
 
 
-class mock_settings_test_extrapolation1:
+class extrapolation_closest:
     CURRENCY_CONVERTER_INTERPOLATION = None
     CURRENCY_CONVERTER_EXTRAPOLATION = "closest"
     CURRENCY_CONVERTER_INTERPOLATION_MAX_DAYS_FALLBACK = -1
     CURRENCY_CONVERTER_EXTRAPOLATION_MAX_DAYS_FALLBACK = -1
 
 
-class mock_settings_test_extrapolation2:
+class extrapolation_closest_with_max_days:
     CURRENCY_CONVERTER_INTERPOLATION = None
     CURRENCY_CONVERTER_EXTRAPOLATION = "closest"
     CURRENCY_CONVERTER_INTERPOLATION_MAX_DAYS_FALLBACK = -1
@@ -130,7 +130,7 @@ item_test_extrapolation = [
 
 
 def test_extrapolation():
-    with patch.object(cc, "settings", new=mock_settings_test_extrapolation1):
+    with patch.object(cc, "settings", new=extrapolation_closest):
         cc.import_data(item_test_extrapolation)
 
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 1)) == 3
@@ -140,7 +140,7 @@ def test_extrapolation():
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 5)) == 4
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 6)) == 4
 
-    with patch.object(cc, "settings", new=mock_settings_test_extrapolation2):
+    with patch.object(cc, "settings", new=extrapolation_closest_with_max_days):
         cc.import_data(item_test_extrapolation)
 
         assert cc.convert(1, "CZK", "HNL", date(2019, 1, 1)) is None
