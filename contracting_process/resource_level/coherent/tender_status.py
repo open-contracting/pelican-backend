3444ed6eca92e7b3b2ad33c1ecbf94b2dc1cbd3b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-10-29 19:59:59 -0400

    fix: tender_status now fails on non-zero length arrays (was passing if all entries were blank)
    The default behavior of OCDS Merge is to remove empty objects from arrays.
    
      import ocdsmerge
      ocdsmerge.Merger().create_compiled_release([{"awards":[{},{"title": ""}]}])
    
    As such, cases like `[{}, {}]` should never occur.

--- contracting_process/resource_level/coherent/tender_status.py
+++ contracting_process/resource_level/coherent/tender_status.py
@@ -1,3 +1,8 @@
+"""
+If tender.status is incomplete ('planning', 'planned', 'active', 'cancelled', 'unsuccessful' or 'withdrawn'), then
+awards and contracts are blank.
+"""
+
 from tools.checks import get_empty_result_resource
 from tools.getter import get_values
 
@@ -8,46 +13,27 @@ def calculate(item):
     result = get_empty_result_resource(version)
 
     tender_status = get_values(item, "tender.status", value_only=True)
-
     if not tender_status:
-        # completely missing tender value
-        result["result"] = None
-        result["meta"] = {"reason": "criteria not met"}
+        result["meta"] = {"reason": "tender.status is not present"}
         return result
 
     tender_status = tender_status[0]
-
-    if tender_status in ("planning", "planned", "active", "cancelled", "unsuccessful", "withdrawn"):
-        contracts = get_values(item, "contracts", value_only=True)
-        awards = get_values(item, "awards", value_only=True)
-
-        contracts_count = 0
-        if contracts:
-            for contract in contracts:
-                if contract:
-                    contracts_count = contracts_count + 1
-
-        awards_count = 0
-        if awards:
-            for award in awards:
-                if award:
-                    awards_count = awards_count + 1
-
-        if contracts_count > 0 or awards_count > 0:
-            result["result"] = False
-            result["application_count"] = 1
-            result["pass_count"] = 0
-            result["meta"] = {"contracts_count": contracts_count, "awards_count": awards_count}
-            return result
-
-        result["result"] = True
-        result["application_count"] = 1
-        result["pass_count"] = 1
-        result["meta"] = None
-
+    if tender_status not in ("planning", "planned", "active", "cancelled", "unsuccessful", "withdrawn"):
+        result["meta"] = {"reason": f"tender.status is {tender_status!r}"}
         return result
 
-    # unable to compare, undefined
-    result["result"] = None
-    result["meta"] = {"reason": "criteria not met"}
+    contracts_count = len(get_values(item, "contracts", value_only=True))
+    awards_count = len(get_values(item, "awards", value_only=True))
+
+    if contracts_count or awards_count:
+        result["result"] = False
+        result["application_count"] = 1
+        result["pass_count"] = 0
+        result["meta"] = {"contracts_count": contracts_count, "awards_count": awards_count}
+        return result
+
+    result["result"] = True
+    result["application_count"] = 1
+    result["pass_count"] = 1
+    result["meta"] = None
     return result
--- tests/compiled_release/coherent/test_tender_status.py
+++ tests/compiled_release/coherent/test_tender_status.py
@@ -9,27 +9,28 @@ class TestCase(CompiledReleaseTests, unittest.TestCase):
     skipping = [
         (
             {"tender": {}},
-            "criteria not met",
+            "tender.status is not present",
         ),
         (
-            {"tender": {"status": "invalid"}},  # invalid
-            "criteria not met",
+            {"tender": {"status": "complete"}},
+            "tender.status is 'complete'",
         ),
     ]
     passing = [
         ({"tender": {"status": "planning"}}, None, 1),
         ({"tender": {"status": "planning"}, "awards": []}, None, 1),
+        ({"tender": {"status": "planning"}, "contracts": []}, None, 1),
         ({"tender": {"status": "planning"}, "awards": [], "contracts": []}, None, 1),
     ]
     failing = [
         (
-            {"tender": {"status": "planning"}, "awards": [{"title": ""}]},
+            {"tender": {"status": "planning"}, "awards": [{}]},
             {"contracts_count": 0, "awards_count": 1},
             1,
             0,
         ),
         (
-            {"tender": {"status": "planning"}, "awards": [], "contracts": [{"title": ""}]},
+            {"tender": {"status": "planning"}, "contracts": [{}]},
             {"contracts_count": 1, "awards_count": 0},
             1,
             0,
