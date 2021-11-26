5433942621de837db4e3c4bd43e443e52f9e31fe
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-11-26 12:12:16 -0500

    tools(exchange_rates_db): Retrieve symbols from API, instead of hardcoding

--- tools/exchange_rates_db.py
+++ tools/exchange_rates_db.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from datetime import date, timedelta
 
 import psycopg2
@@ -6,17 +7,13 @@ import requests
 
 from tools import settings
 from tools.db import commit, get_cursor, rollback
-from tools.logging_helper import get_logger
 
-LINK = "http://data.fixer.io/api/{date_historical}?access_key={access_key}&base={base}&symbols={symbols}"
-CURRENCIES = (
-    "AED,AFN,ALL,AMD,ANG,AOA,ARS,AUD,AWG,AZN,BAM,BBD,BDT,BGN,BHD,BIF,BMD,BND,BOB,BRL,BSD,BTC,BTN,BWP,BYR,BYN,BZD,CAD,"
-    "CDF,CHF,CLF,CLP,CNY,COP,CRC,CUC,CUP,CVE,CZK,DJF,DKK,DOP,DZD,EGP,ERN,ETB,EUR,FJD,FKP,GBP,GEL,GGP,GHS,GIP,GMD,GNF,"
-    "GTQ,GYD,HKD,HNL,HRK,HTG,HUF,IDR,ILS,IMP,INR,IQD,IRR,ISK,JEP,JMD,JOD,JPY,KES,KGS,KHR,KMF,KPW,KRW,KWD,KYD,KZT,LAK,"
-    "LBP,LKR,LRD,LSL,LTL,LVL,LYD,MAD,MDL,MGA,MKD,MMK,MNT,MOP,MRO,MUR,MVR,MWK,MXN,MYR,MZN,NAD,NGN,NIO,NOK,NPR,NZD,OMR,"
-    "PAB,PEN,PGK,PHP,PKR,PLN,PYG,QAR,RON,RSD,RUB,RWF,SAR,SBD,SCR,SDG,SEK,SGD,SHP,SLL,SOS,SRD,STD,SVC,SYP,SZL,THB,TJS,"
-    "TMT,TND,TOP,TRY,TTD,TWD,TZS,UAH,UGX,USD,UYU,UZS,VEF,VND,VUV,WST,XAF,XAG,XAU,XCD,XDR,XOF,XPF,YER,ZAR,ZMK,ZMW,ZWL"
-)
+logger = logging.getLogger(__name__)
+
+# Datlab had already downloaded the exchange rates to EUR from another project. Changing the base would require
+# re-downloading decades of exchange rates. It makes no difference to the application's logic, as all currency
+# operations are performed in USD.
+FIXER_IO_URL = "https://data.fixer.io/api/{date}?access_key={access_key}&base=EUR&symbols={symbols}"
 
 
 def load():
@@ -26,7 +23,6 @@ def load():
 
 
 def update_from_fixer_io():
-    logger = get_logger()
     logger.info("Starting currency exchange rates update.")
 
     with get_cursor() as cursor:
@@ -38,35 +34,49 @@ def update_from_fixer_io():
 
         max_date = query_result[0]
         date_now = date.today()
-        logger.info("Last available date is %s. %s day(s) will be updated.", max_date, (date_now - max_date).days)
+        length = (date_now - max_date).days
+
+        if not length:
+            logger.info("Last available date is %s. Nothing to update.", max_date)
+            return
+
+        logger.info("Last available date is %s. %s date(s) will be updated.", max_date, length)
+        access_key = settings.FIXER_IO_API_KEY
+
+        try:
+            response = requests.get(f"https://data.fixer.io/api/symbols?access_key={access_key}")
+            response.raise_for_status()
+        except requests.RequestException as e:
+            logger.error("Couldn't retrieve currency symbols: %s", e)
+            return
+
+        data = response.json()
+        if not data["success"] or "symbols" not in data:
+            logger.error("API request for currency symbols did not succeed.")
+            return
+
+        symbols = ",".join(data["symbols"])
 
         target_date = max_date
+        # Don't retrieve today's exchange rates, because the data might be incomplete.
         while target_date < date_now:
             try:
                 date_str = target_date.strftime("%Y-%m-%d")
                 logger.info("Fetching exchange rates for %s.", date_str)
 
                 response = requests.get(
-                    LINK.format(
-                        date_historical=date_str,
-                        access_key=settings.FIXER_IO_API_KEY,
-                        # Datlab had already downloaded the exchange rates to EUR from another project. Changing the
-                        # base would require re-downloading decades of exchange rates. It makes no difference to the
-                        # application's logic, as all currency operations are performed in USD.
-                        base="EUR",
-                        symbols=CURRENCIES,
-                    ),
+                    FIXER_IO_URL.format(date=date_str, access_key=access_key, symbols=symbols),
                     timeout=10,
                 )
                 response.raise_for_status()
             except requests.RequestException as e:
-                logger.error("Couldn't connect to fixer.io: %s", e)
+                logger.error("Couldn't retrieve currency rates: %s", e)
                 break
 
             try:
                 data = response.json()
                 if not data["success"] or "rates" not in data:
-                    logger.error("Failed to successfully fetch exchange rates for %s.", date_str)
+                    logger.error("API request for currency rates on %s did not succeed.", date_str)
                     break
 
                 cursor.execute(
