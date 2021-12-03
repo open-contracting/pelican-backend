a2f14a6b28d085a2e55539cdcb31a26f4e9f679a
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-12-03 13:48:50 -0500

    chore: Rename finish_worker to finish_callback (because the worker continues once the callback is finished)

--- workers/check/dataset.py
+++ workers/check/dataset.py
@@ -7,7 +7,7 @@ from yapw.methods.blocking import ack, nack
 from dataset import processor
 from tools import settings
 from tools.currency_converter import bootstrap
-from tools.helpers import finish_worker, is_step_required
+from tools.helpers import finish_callback, is_step_required
 from tools.services import commit, create_client
 from tools.state import (
     get_dataset_progress,
@@ -36,56 +36,37 @@ def callback(client_state, channel, method, properties, input_message):
     dataset_id = input_message["dataset_id"]
 
     if not is_step_required(settings.Steps.DATASET):
-        finish_worker(client_state, channel, method, dataset_id, phase.DATASET, routing_key=routing_key)
+        finish_callback(client_state, channel, method, dataset_id, phase.DATASET, routing_key=routing_key)
         return
 
     delivery_tag = method.delivery_tag
 
     dataset = get_dataset_progress(dataset_id)
 
-    # optimization when resending
-    if dataset["state"] == state.OK and dataset["phase"] == phase.DATASET:
-        logger.info("Checks have been already calculated for this dataset.")
+    if dataset["phase"] == phase.CONTRACTING_PROCESS and dataset["state"] == state.IN_PROGRESS:
+        logger.info("CONTRACTING_PROCESS phase still in-progress for dataset_id %s", dataset_id)
+        ack(client_state, channel, delivery_tag)
+        return
+    if dataset["phase"] == phase.DATASET and dataset["state"] == state.IN_PROGRESS:
+        logger.info("DATASET phase already in-progress for dataset_id %s", dataset_id)
+        ack(client_state, channel, delivery_tag)
+        return
+    if dataset["phase"] in (phase.DATASET, phase.TIME_VARIANCE, phase.CHECKED, phase.DELETED):
+        logger.info("DATASET phase already complete for dataset_id %s", dataset_id)
         ack(client_state, channel, delivery_tag)
         return
 
-    if dataset["state"] == state.IN_PROGRESS and dataset["phase"] == phase.DATASET:
-        # lets do nothing, calculations is already in progress
-        logger.info("Other worker probably already started with the job. Doing nothing.")
-        ack(client_state, channel, delivery_tag)
-        return
-
-    if dataset["phase"] == phase.TIME_VARIANCE or dataset["phase"] == phase.CHECKED:
-        logger.info("Checks have been already calculated for this dataset.")
-        ack(client_state, channel, delivery_tag)
-        return
-
-    if dataset["state"] == state.IN_PROGRESS and dataset["phase"] == phase.CONTRACTING_PROCESS:
-        # contracting process is not done yet
-        logger.info("Not all messages have been processed by contracting process.")
-        ack(client_state, channel, delivery_tag)
-        return
-
+    # Implement the Aggregator pattern.
     processed_count = get_processed_items_count(dataset_id)
     total_count = get_total_items_count(dataset_id)
+    difference = total_count - processed_count
 
-    # check whether all items are processed
-    if processed_count < total_count:
-        logger.debug(
-            "There are %s remaining messages to be processed for dataset_id %s",
-            total_count - processed_count,
-            dataset_id,
-        )
-
-        logger.info("Not all messages have been processed by contracting process.")
+    if difference:
+        logger.info("CONTRACTING_PROCESS phase has %s messages left for dataset_id %s", difference, dataset_id)
         ack(client_state, channel, delivery_tag)
         return
 
-    if (
-        dataset["state"] == state.OK
-        and dataset["phase"] == phase.CONTRACTING_PROCESS
-        and processed_count == total_count
-    ):
+    if dataset["phase"] == phase.CONTRACTING_PROCESS and dataset["state"] == state.OK and not difference:
         logger.info(
             "All messages for dataset_id %s with %s items processed, starting to calculate dataset level checks",
             dataset_id,
@@ -97,7 +78,7 @@ def callback(client_state, channel, method, properties, input_message):
 
         processor.do_work(dataset_id, logger)
 
-        finish_worker(client_state, channel, method, dataset_id, phase.DATASET, routing_key=routing_key)
+        finish_callback(client_state, channel, method, dataset_id, phase.DATASET, routing_key=routing_key)
     else:
         logger.error(
             "Dataset processing for dataset_id %s is in weird state. Dataset state %s. Dataset phase %s.",
