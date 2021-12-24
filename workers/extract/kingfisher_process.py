open-contracting/pelican-backend

commit 59414070c6e06d3a4a94efca6fcc48a9a0745c53

chore: Make extract workers more comparable. Set item states in batches. Set dataset state once, not twice. Use unique index inference instead of naming a constraint. https://www.postgresql.org/docs/12/sql-insert.html

workers/extract/kingfisher_process.py
@@ -10,7 +10,7 @@ from yapw.methods.blocking import ack, publish
 import dataset.meta_data_aggregator as meta_data_aggregator
 from tools import exchange_rates_db, settings
 from tools.services import commit, create_client, get_cursor
-from tools.state import phase, set_dataset_state, set_item_state, state
+from tools.state import phase, set_dataset_state, set_items_state, state
 
 consume_routing_key = "ocds_kingfisher_extractor_init"
 routing_key = "extractor"
@@ -96,7 +96,7 @@ def callback(client_state, channel, method, properties, input_message):
         batch = []
 
         i = 0
-        items_inserted = 0
+        inserts = 0
         items_count = len(result)
         while i * page_size < items_count:
             ids = []
@@ -114,21 +114,20 @@ def callback(client_state, channel, method, properties, input_message):
 
             for row in cursor.fetchall():
                 inserted_id = row[0]
-
-                items_inserted += 1
-
-                set_item_state(dataset_id, inserted_id, state.IN_PROGRESS)
-
-                set_dataset_state(dataset_id, state.IN_PROGRESS, phase.CONTRACTING_PROCESS, size=items_inserted)
-
-                if items_inserted == items_count:
-                    set_dataset_state(dataset_id, state.OK, phase.CONTRACTING_PROCESS)
-
-                commit()
-
-                batch_size += 1
                 batch.append(inserted_id)
-                if batch_size >= max_batch_size or items_inserted == items_count:
+                batch_size += 1
+                inserts += 1
+
+                if batch_size >= max_batch_size or inserts == items_count:
+                    set_items_state(dataset_id, batch, state.IN_PROGRESS)
+
+                    if inserts == items_count:
+                        set_dataset_state(dataset_id, state.OK, phase.CONTRACTING_PROCESS)
+                    else:
+                        set_dataset_state(dataset_id, state.IN_PROGRESS, phase.CONTRACTING_PROCESS, size=inserts)
+
+                    commit()
+
                     publish(client_state, channel, {"item_ids": batch, "dataset_id": dataset_id}, routing_key)
 
                     batch_size = 0
@@ -138,11 +137,11 @@ def callback(client_state, channel, method, properties, input_message):
                 "Inserted page %s from %s. %s items out of %s downloaded",
                 i,
                 ceil(float(items_count) / float(page_size)),
-                items_inserted,
+                inserts,
                 items_count,
             )
 
-        logger.info("All items with dataset_id %s have been downloaded", dataset_id)
+        logger.info("Done extracting data to dataset %s", dataset_id)
         kf_cursor.close()
         kf_connection.close()
     finally:

contracting_process/processor.py
@@ -10,13 +10,13 @@ from tools import settings
 from tools.getter import get_values
 from tools.helpers import is_step_required
 from tools.services import get_cursor
-from tools.state import set_item_state, state
+from tools.state import set_items_state, state
 
 logger = logging.getLogger("pelican.contracting_process.processor")
 
 
-# item: (data, item_id, dataset_id)
-def do_work(items):
+# item: (data, item_id)
+def do_work(dataset_id, items):
     field_level_check_results = []
     resource_level_check_results = []
 
@@ -29,11 +29,11 @@ def do_work(items):
         items_count += 1
 
         if do_field_level:
-            field_level_check_results.append(field_level_checks(*item, do_field_quality=do_field_quality))
+            field_level_check_results.append(field_level_checks(*item, dataset_id, do_field_quality=do_field_quality))
         if do_resource_level:
-            resource_level_check_results.append(resource_level_checks(*item))
+            resource_level_check_results.append(resource_level_checks(*item, dataset_id))
 
-        set_item_state(item[2], item[1], state.OK)
+    set_items_state(dataset_id, [item[1] for item in items], state.OK)
 
     if do_field_level:
         save_field_level_checks(field_level_check_results, items_count)

tools/state.py
@@ -1,4 +1,6 @@
-from typing import Any, Optional, Tuple
+from typing import Any, List, Optional, Tuple
+
+from psycopg2.extras import execute_values
 
 from tools.services import get_cursor
 
@@ -29,38 +31,36 @@ def set_dataset_state(dataset_id: int, state: str, phase: str, size: Optional[in
         sql = """\
             INSERT INTO progress_monitor_dataset (dataset_id, state, phase, size)
             VALUES (%(dataset_id)s, %(state)s, %(phase)s, %(size)s)
-            ON CONFLICT ON CONSTRAINT unique_dataset_id
+            ON CONFLICT (dataset_id)
             DO UPDATE SET state = %(state)s, phase = %(phase)s, size = %(size)s, modified = now()
         """
     else:
         sql = """\
             INSERT INTO progress_monitor_dataset (dataset_id, state, phase, size)
             VALUES (%(dataset_id)s, %(state)s, %(phase)s, %(size)s)
-            ON CONFLICT ON CONSTRAINT unique_dataset_id
+            ON CONFLICT (dataset_id)
             DO UPDATE SET state = %(state)s, phase = %(phase)s, modified = now()
         """
     with get_cursor() as cursor:
         cursor.execute(sql, {"dataset_id": dataset_id, "state": state, "phase": phase, "size": size})
 
 
-def set_item_state(dataset_id: int, item_id: int, state: str) -> None:
+def set_items_state(dataset_id: int, item_ids: List[int], state: str) -> None:
     """
-    Upsert a data item's progress to the given state.
+    Upsert data items' progress to the given state.
 
     :param dataset_id: the dataset's ID
-    :param item_id: the data item's iD
+    :param item_ids: the data items' IDs
     :param state: the state to set
     """
     with get_cursor() as cursor:
-        cursor.execute(
-            """\
+        sql = """\
             INSERT INTO progress_monitor_item (dataset_id, item_id, state)
-            VALUES (%(dataset_id)s, %(item_id)s, %(state)s)
-            ON CONFLICT ON CONSTRAINT unique_dataset_id_item_id
-            DO UPDATE SET state = %(state)s, modified = now()
-            """,
-            {"dataset_id": dataset_id, "item_id": item_id, "state": state},
-        )
+            VALUES %s
+            ON CONFLICT (dataset_id, item_id)
+            DO UPDATE SET state = excluded.state, modified = now()
+        """
+        execute_values(cursor, sql, [(dataset_id, item_id, state) for item_id in item_ids])
 
 
 def get_processed_items_count(dataset_id: int) -> int:

workers/check/data_item.py
@@ -24,7 +24,7 @@ def callback(client_state, channel, method, properties, input_message):
     item_ids = input_message["item_ids"]
 
     with get_cursor() as cursor:
-        cursor.execute("SELECT data, id, dataset_id FROM data_item WHERE id IN %(ids)s", {"ids": tuple(item_ids)})
+        cursor.execute("SELECT data, id FROM data_item WHERE id IN %(ids)s", {"ids": tuple(item_ids)})
         processor.do_work(cursor.fetchall())
 
     commit()

workers/extract/dataset_filter.py
@@ -9,7 +9,7 @@ from yapw.methods.blocking import ack, publish
 
 from tools import settings
 from tools.services import commit, create_client, get_cursor
-from tools.state import phase, set_dataset_state, set_item_state, state
+from tools.state import phase, set_dataset_state, set_items_state, state
 
 consume_routing_key = "dataset_filter_extractor_init"
 routing_key = "extractor"
@@ -151,7 +151,7 @@ def callback(client_state, channel, method, properties, input_message):
         batch = []
 
         i = 0
-        items_inserted = 0
+        inserts = 0
         items_count = len(ids)
         while i * page_size < items_count:
             page_ids = ids[i * page_size : (i + 1) * page_size]
@@ -169,18 +169,22 @@ def callback(client_state, channel, method, properties, input_message):
 
             for row in cursor.fetchall():
                 inserted_id = row[0]
-                items_inserted += 1
-                set_item_state(dataset_id_filtered, inserted_id, state.IN_PROGRESS)
-                set_dataset_state(
-                    dataset_id_filtered, state.IN_PROGRESS, phase.CONTRACTING_PROCESS, size=items_inserted
-                )
-                if items_inserted == items_count:
-                    set_dataset_state(dataset_id_filtered, state.OK, phase.CONTRACTING_PROCESS)
-                commit()
-
-                batch_size += 1
                 batch.append(inserted_id)
-                if batch_size >= max_batch_size or items_inserted == items_count:
+                batch_size += 1
+                inserts += 1
+
+                if batch_size >= max_batch_size or inserts == items_count:
+                    set_items_state(dataset_id_filtered, batch, state.IN_PROGRESS)
+
+                    if inserts == items_count:
+                        set_dataset_state(dataset_id_filtered, state.OK, phase.CONTRACTING_PROCESS)
+                    else:
+                        set_dataset_state(
+                            dataset_id_filtered, state.IN_PROGRESS, phase.CONTRACTING_PROCESS, size=inserts
+                        )
+
+                    commit()
+
                     publish(client_state, channel, {"item_ids": batch, "dataset_id": dataset_id_filtered}, routing_key)
 
                     batch_size = 0
@@ -190,15 +194,11 @@ def callback(client_state, channel, method, properties, input_message):
                 "Inserted page %s from %s. %s items out of %s downloaded",
                 i,
                 ceil(float(items_count) / float(page_size)),
-                items_inserted,
+                inserts,
                 items_count,
             )
 
-        logger.info(
-            "All original items with dataset_id %s have been duplicated with dataset_id %s",
-            dataset_id_original,
-            dataset_id_filtered,
-        )
+        logger.info("Done filtering dataset %s to dataset %s", dataset_id_original, dataset_id_filtered)
     finally:
         cursor.close()
 

commit efea84dec59345cf3338adc6f0a2b1c042694e01

fix: Set size on completion (bug I introduced in 59414070c6e06d3a4a94efca6fcc48a9a0745c53)

workers/extract/kingfisher_process.py
@@ -122,7 +122,7 @@ def callback(client_state, channel, method, properties, input_message):
                     set_items_state(dataset_id, batch, state.IN_PROGRESS)
 
                     if inserts == items_count:
-                        set_dataset_state(dataset_id, state.OK, phase.CONTRACTING_PROCESS)
+                        set_dataset_state(dataset_id, state.OK, phase.CONTRACTING_PROCESS, size=inserts)
                     else:
                         set_dataset_state(dataset_id, state.IN_PROGRESS, phase.CONTRACTING_PROCESS, size=inserts)
 