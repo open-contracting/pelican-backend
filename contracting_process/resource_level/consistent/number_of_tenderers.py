f5513b10473bacf4f25115f0f8070b1ec353b332
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-01-19 16:42:49 -0500

    chore: consistent.test_number_of_tenderers: Use deep_get #75

--- contracting_process/resource_level/consistent/number_of_tenderers.py
+++ contracting_process/resource_level/consistent/number_of_tenderers.py
@@ -5,6 +5,7 @@ The test is skipped if the ``tenderers`` field is not an array.
 """
 
 from tools.checks import complete_result_resource_pass_fail, get_empty_result_resource
+from tools.getter import deep_get
 
 version = 1.0
 
@@ -12,18 +13,19 @@ version = 1.0
 def calculate(item):
     result = get_empty_result_resource(version)
 
-    if "tender" not in item:
-        result["meta"] = {"reason": "tender is not set"}
+    number_of_tenderers = deep_get(item, "tender.numberOfTenderers", float)
+    if number_of_tenderers is None:
+        result["meta"] = {"reason": "numberOfTenderers is non-numeric"}
         return result
 
-    tender = item["tender"]
+    tenderers = deep_get(item, "tender.tenderers")
 
-    if "tenderers" not in tender or "numberOfTenderers" not in tender or type(tender["tenderers"]) != list:
-        result["meta"] = {"reason": "numberOfTenderers is not set or tenderers is not a list"}
+    if type(tenderers) != list:
+        result["meta"] = {"reason": "tenderers is not an array"}
         return result
 
     return complete_result_resource_pass_fail(
         result,
-        tender["numberOfTenderers"] == len(tender["tenderers"]),
-        {"numberOfTenderers": tender["numberOfTenderers"], "tenderers": tender["tenderers"]},
+        number_of_tenderers == len(tenderers),
+        {"numberOfTenderers": item["tender"]["numberOfTenderers"], "tenderers": item["tender"]["tenderers"]},
     )
