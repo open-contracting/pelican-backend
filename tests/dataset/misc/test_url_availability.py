e31b2806a1afe082d426234c07fcb86312c228e5
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-06-15 00:27:15 -0400

    test: Relax expectation (presumably even the Docker image can fail requests)

--- tests/dataset/misc/test_url_availability.py
+++ tests/dataset/misc/test_url_availability.py
@@ -71,8 +71,8 @@ def test_failed_multiple():
 
         result = url_availability.get_result(scope)
         assert result["result"] is False
-        assert result["value"] >= 99
-        assert len(result["meta"]["passed_examples"]) == 99
-        assert len(result["meta"]["failed_examples"]) == 1
-        assert len([example for example in result["meta"]["passed_examples"] if example["status"] == "OK"]) == 99
-        assert len([example for example in result["meta"]["failed_examples"] if example["status"] == "ERROR"]) == 1
+        assert result["value"] >= 98
+        assert len(result["meta"]["passed_examples"]) >= 98
+        assert len(result["meta"]["failed_examples"]) <= 2
+        assert len([example for example in result["meta"]["passed_examples"] if example["status"] == "OK"]) >= 98
+        assert len([example for example in result["meta"]["failed_examples"] if example["status"] == "ERROR"]) <= 2
