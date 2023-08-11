8f4c1fa51fcd4f82358540f708a8492f53e8d918
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-08-11 15:30:50 -0400

    chore(distribution.code_distribution): Code review and add wiki content #3

--- dataset/distribution/code_distribution.py
+++ dataset/distribution/code_distribution.py
@@ -1,3 +1,9 @@
+"""
+If ``test_values`` is set, then each test value occurs in between 0.1% and 99% of cases.
+
+Otherwise, no test is performed.
+"""
+
 from pelican.util.checks import ReservoirSampler, get_empty_result_dataset
 from pelican.util.getter import get_values
 
@@ -9,25 +15,14 @@ class CodeDistribution:
         self.limit = limit
 
     def add_item(self, scope, item, item_id):
-        ocid = get_values(item, "ocid", value_only=True)[0]
+        ocid = item["ocid"]
 
-        values = []
         for path in self.paths:
-            values.extend(get_values(item, path, value_only=True))
-
-        for value in values:
-            if value is None or not isinstance(value, str):
-                continue
-
-            scope.setdefault(
-                value,
-                {
-                    "count": 0,
-                    "sampler": ReservoirSampler(self.limit),
-                },
-            )
-            scope[value]["count"] += 1
-            scope[value]["sampler"].process({"item_id": item_id, "ocid": ocid})
+            # Use get_values(), since path can be to either a string or a list of strings.
+            for value in get_values(item, path, value_only=True):
+                if type(value) is str:
+                    scope.setdefault(value, ReservoirSampler(self.limit))
+                    scope[value].process({"item_id": item_id, "ocid": ocid})
 
         return scope
 
@@ -38,22 +33,22 @@ class CodeDistribution:
             result["meta"] = {"reason": "no compiled releases set necessary fields"}
             return result
 
-        total_count = sum(value["count"] for value in scope.values())
-
+        total_count = sum(sampler.index for sampler in scope.values())
         passed = True
-        for key, value in scope.items():
-            value["share"] = value["count"] / total_count
-            value["examples"] = value["sampler"].sample
-            del value["sampler"]
+        shares = {}
 
-            if key in self.test_values:
-                passed = passed and (0.001 <= value["share"] <= 0.99)
+        for code, sampler in scope.items():
+            share = sampler.index / total_count
+            if code in self.test_values:
+                passed = passed and (0.001 <= share <= 0.99)
+            shares[code] = {"count": sampler.index, "share": share, "examples": sampler.sample}
 
-        if any(key not in scope for key in self.test_values):
+        # In other words, its share is 0%.
+        if any(code not in scope for code in self.test_values):
             passed = False
 
         result["result"] = passed
         result["value"] = 100 if passed else 0
-        result["meta"] = {"shares": scope}
+        result["meta"] = {"shares": shares}
 
         return result
