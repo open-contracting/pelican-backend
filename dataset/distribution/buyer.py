c92f3f015ea36d285079c0f7bf78f7892bccb416
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-08-11 16:09:54 -0400

    chore(distribution.buyer, distribution.buyer_repetition): Use deep_get instead of get_values #75. Add wiki/frontend content #3.

--- dataset/distribution/buyer.py
+++ dataset/distribution/buyer.py
@@ -1,5 +1,13 @@
+"""
+Fewer than 50% of all buyers are identified in only one compiled release. Failure indicates issues in buyer
+identification. Buyers are identified by ``buyer.identifier.scheme`` and ``buyer.identifier.id``.
+
+The test is skipped if the ``buyer.identifier.scheme`` and ``buyer.identifier.id`` fields are both present in fewer
+than 1,000 compiled releases.
+"""
+
 from pelican.util.checks import ReservoirSampler, get_empty_result_dataset
-from pelican.util.getter import get_values
+from pelican.util.getter import deep_get
 
 version = 1.0
 min_items = 1000
@@ -10,28 +18,17 @@ def add_item(scope, item, item_id):
     if not scope:
         scope = {"buyers": {}, "total_ocid_count": 0}
 
-    # filtering values containing required fields
-    values = get_values(item, "buyer.identifier", value_only=True)
-    if not values:
+    scheme = deep_get(item, "buyer.identifier.scheme", str)
+    ident = deep_get(item, "buyer.identifier.id", str)
+    if not scheme or not ident:
         return scope
 
-    buyer_identifier = values[0]
-    if (
-        "scheme" not in buyer_identifier
-        or "id" not in buyer_identifier
-        or buyer_identifier["scheme"] is None
-        or buyer_identifier["id"] is None
-    ):
-        return scope
-
-    ocid = get_values(item, "ocid", value_only=True)[0]
-    scope["total_ocid_count"] += 1
-    key = (buyer_identifier["scheme"], buyer_identifier["id"])
-
-    if key not in scope["buyers"]:
-        scope["buyers"][key] = {"total_ocid_count": 1, "example": {"item_id": item_id, "ocid": ocid}}
+    identifier = (scheme, ident)
+    if identifier not in scope["buyers"]:
+        scope["buyers"][identifier] = {"count": 1, "example": {"item_id": item_id, "ocid": item["ocid"]}}
     else:
-        scope["buyers"][key]["total_ocid_count"] += 1
+        scope["buyers"][identifier]["count"] += 1
+    scope["total_ocid_count"] += 1
 
     return scope
 
@@ -43,11 +40,11 @@ def get_result(scope):
         result["meta"] = {"reason": "no compiled releases set necessary fields"}
         return result
 
-    if scope["total_ocid_count"] < min_items:
+    total_ocid_count = scope["total_ocid_count"]
+    if total_ocid_count < min_items:
         result["meta"] = {"reason": f"fewer than {min_items} occurrences of necessary fields"}
         return result
 
-    # initializing histogram
     ocid_histogram = {
         "1": {"total_ocid_count": 0, "total_buyer_count": 0},
         "2_20": {"total_ocid_count": 0, "total_buyer_count": 0},
@@ -55,41 +52,36 @@ def get_result(scope):
         "51_100": {"total_ocid_count": 0, "total_buyer_count": 0},
         "100+": {"total_ocid_count": 0, "total_buyer_count": 0},
     }
+    total_buyer_count = len(scope["buyers"])
+    sampler = ReservoirSampler(sample_size)
 
-    buyer_with_one_ocid_sampler = ReservoirSampler(sample_size)
-
-    # filling in the histogram
     for value in scope["buyers"].values():
-        if value["total_ocid_count"] == 1:
-            ocid_histogram["1"]["total_ocid_count"] += value["total_ocid_count"]
+        if value["count"] == 1:
+            ocid_histogram["1"]["total_ocid_count"] += value["count"]
             ocid_histogram["1"]["total_buyer_count"] += 1
-            buyer_with_one_ocid_sampler.process(value["example"])
-
-        elif 2 <= value["total_ocid_count"] <= 20:
-            ocid_histogram["2_20"]["total_ocid_count"] += value["total_ocid_count"]
+            sampler.process(value["example"])
+        elif 2 <= value["count"] <= 20:
+            ocid_histogram["2_20"]["total_ocid_count"] += value["count"]
             ocid_histogram["2_20"]["total_buyer_count"] += 1
-
-        elif 21 <= value["total_ocid_count"] <= 50:
-            ocid_histogram["21_50"]["total_ocid_count"] += value["total_ocid_count"]
+        elif 21 <= value["count"] <= 50:
+            ocid_histogram["21_50"]["total_ocid_count"] += value["count"]
             ocid_histogram["21_50"]["total_buyer_count"] += 1
-
-        elif 51 <= value["total_ocid_count"] <= 100:
-            ocid_histogram["51_100"]["total_ocid_count"] += value["total_ocid_count"]
+        elif 51 <= value["count"] <= 100:
+            ocid_histogram["51_100"]["total_ocid_count"] += value["count"]
             ocid_histogram["51_100"]["total_buyer_count"] += 1
-
         else:
-            ocid_histogram["100+"]["total_ocid_count"] += value["total_ocid_count"]
+            ocid_histogram["100+"]["total_ocid_count"] += value["count"]
             ocid_histogram["100+"]["total_buyer_count"] += 1
 
-    passed = ocid_histogram["1"]["total_buyer_count"] < 0.5 * len(scope["buyers"])
+    passed = ocid_histogram["1"]["total_buyer_count"] < 0.5 * total_buyer_count
 
     result["result"] = passed
     result["value"] = 100 if passed else 0
     result["meta"] = {
         "counts": ocid_histogram,
-        "total_ocid_count": scope["total_ocid_count"],
-        "total_buyer_count": len(scope["buyers"]),
-        "examples": buyer_with_one_ocid_sampler.sample,
+        "total_ocid_count": total_ocid_count,
+        "total_buyer_count": total_buyer_count,
+        "examples": sampler.sample,
     }
 
     return result
--- dataset/distribution/buyer_repetition.py
+++ dataset/distribution/buyer_repetition.py
@@ -1,5 +1,13 @@
+"""
+The most common buyer is identified in 1% to 50% of compiled releases. Failure indicates issues in buyer identification
+or buyer over-representation. Buyers are identified by ``buyer.identifier.scheme`` and ``buyer.identifier.id``.
+
+The test is skipped if the ``buyer.identifier.scheme`` and ``buyer.identifier.id`` fields are both present in fewer
+than 1,000 compiled releases.
+"""
+
 from pelican.util.checks import ReservoirSampler, get_empty_result_dataset
-from pelican.util.getter import get_values
+from pelican.util.getter import deep_get
 
 version = 1.0
 min_items = 1000
@@ -10,33 +18,15 @@ def add_item(scope, item, item_id):
     if not scope:
         scope = {"buyers": {}, "total_ocid_count": 0}
 
-    # filtering values containing required fields
-    values = get_values(item, "buyer.identifier", value_only=True)
-    if not values:
+    scheme = deep_get(item, "buyer.identifier.scheme", str)
+    ident = deep_get(item, "buyer.identifier.id", str)
+    if not scheme or not ident:
         return scope
 
-    buyer_identifier = values[0]
-    if (
-        "scheme" not in buyer_identifier
-        or "id" not in buyer_identifier
-        or buyer_identifier["scheme"] is None
-        or buyer_identifier["id"] is None
-    ):
-        return scope
-
-    ocid = get_values(item, "ocid", value_only=True)[0]
+    identifier = (scheme, ident)
+    scope["buyers"].setdefault(identifier, ReservoirSampler(sample_size))
+    scope["buyers"][identifier].process({"item_id": item_id, "ocid": item["ocid"]})
     scope["total_ocid_count"] += 1
-    key = (buyer_identifier["scheme"], buyer_identifier["id"])
-
-    scope["buyers"].setdefault(
-        key,
-        {
-            "total_ocid_count": 0,
-            "sampler": ReservoirSampler(sample_size),
-        },
-    )
-    scope["buyers"][key]["total_ocid_count"] += 1
-    scope["buyers"][key]["sampler"].process({"item_id": item_id, "ocid": ocid})
 
     return scope
 
@@ -48,25 +38,24 @@ def get_result(scope):
         result["meta"] = {"reason": "no compiled releases set necessary fields"}
         return result
 
-    if scope["total_ocid_count"] < min_items:
+    total_ocid_count = scope["total_ocid_count"]
+    if total_ocid_count < min_items:
         result["meta"] = {"reason": f"fewer than {min_items} occurrences of necessary fields"}
         return result
 
-    biggest_buyer_scheme, biggest_buyer_id = max(
-        scope["buyers"], key=(lambda key: scope["buyers"][key]["total_ocid_count"])
-    )
+    biggest_buyer_scheme, biggest_buyer_id = max(scope["buyers"], key=lambda key: scope["buyers"][key].index)
     biggest_buyer = scope["buyers"][(biggest_buyer_scheme, biggest_buyer_id)]
-
-    passed = (0.01 * scope["total_ocid_count"]) < biggest_buyer["total_ocid_count"] < (0.5 * scope["total_ocid_count"])
+    biggest_buyer_share = biggest_buyer.index / total_ocid_count
+    passed = 0.01 < biggest_buyer_share < 0.5
 
     result["result"] = passed
     result["value"] = 100 if passed else 0
     result["meta"] = {
-        "total_ocid_count": scope["total_ocid_count"],
-        "ocid_count": biggest_buyer["total_ocid_count"],
-        "ocid_share": biggest_buyer["total_ocid_count"] / scope["total_ocid_count"],
-        "examples": biggest_buyer["sampler"].sample,
-        "specifics": {"buyer.identifier.id": biggest_buyer_id, "buyer.identifier.scheme": biggest_buyer_scheme},
+        "total_ocid_count": total_ocid_count,
+        "ocid_count": biggest_buyer.index,
+        "ocid_share": biggest_buyer_share,
+        "examples": biggest_buyer.sample,
+        "specifics": {"buyer.identifier.scheme": biggest_buyer_scheme, "buyer.identifier.id": biggest_buyer_id},
     }
 
     return result
