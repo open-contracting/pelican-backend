eefc17ac3e6ab15d0d9b8ff908533a8bc58895e7
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-05-23 01:06:08 -0400

    test: Add tests for metadata aggregator

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 import json
 import os
 
+import psycopg2.extras
 import pytest
 from jsonschema import FormatChecker
+
+from pelican.util import settings
 
 
 @pytest.fixture(scope="session")
@@ -29,3 +32,151 @@ def schema():
 @pytest.fixture(scope="session")
 def format_checker():
     return FormatChecker()
+
+
+@pytest.fixture(scope="session")
+def kingfisher_process_cursor():
+    connection = psycopg2.connect(settings.KINGFISHER_PROCESS_DATABASE_URL)
+    return connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+@pytest.fixture(scope="session")
+def collection_rows(kingfisher_process_cursor):
+    kingfisher_process_cursor.execute(
+        """\
+        INSERT INTO collection (
+            source_id,
+            data_version,
+            sample,
+            transform_type,
+            scrapyd_job,
+            steps,
+            options,
+            data_type,
+            compilation_started,
+            store_start_at,
+            store_end_at
+        ) VALUES (
+            'pytest',
+            '1970-01-01 00:00:00',
+            false,
+            '',
+            '',
+            '[]'::jsonb,
+            '{}'::jsonb,
+            '{}'::jsonb,
+            false,
+            '2001-02-03 04:05:06',
+            '9999-12-31 23:59:59'
+        )
+        """
+    )
+    kingfisher_process_cursor.execute("SELECT id FROM collection WHERE source_id = 'pytest'")
+    original = kingfisher_process_cursor.fetchone()["id"]
+
+    kingfisher_process_cursor.execute(
+        """\
+        INSERT INTO collection (
+            source_id,
+            data_version,
+            sample,
+            transform_from_collection_id,
+            transform_type,
+            scrapyd_job,
+            steps,
+            options,
+            data_type,
+            compilation_started,
+            store_start_at,
+            store_end_at
+        ) VALUES (
+            'pytest',
+            '1970-01-01 00:00:00',
+            false,
+            %(collection_id)s,
+            'compile-releases',
+            '',
+            '[]'::jsonb,
+            '{}'::jsonb,
+            '{}'::jsonb,
+            false,
+            '0001-01-01 00:00:00',
+            '2001-02-03 07:08:09'
+        )
+        """,
+        {"collection_id": original},
+    )
+    kingfisher_process_cursor.execute("SELECT id FROM collection WHERE source_id = 'pytest' AND transform_type <> ''")
+    compiled = kingfisher_process_cursor.fetchone()["id"]
+
+    return original, compiled
+
+
+@pytest.fixture(scope="session")
+def collection_file_item(kingfisher_process_cursor, collection_rows):
+    kingfisher_process_cursor.execute(
+        """\
+        INSERT INTO collection_file (
+            filename,
+            url,
+            collection_id
+        ) VALUES (
+            'test.json',
+            'https://example.com/test.json',
+            %(collection_id)s
+        )
+        """,
+        {"collection_id": collection_rows[1]},
+    )
+    kingfisher_process_cursor.execute("SELECT MAX(id) FROM collection_file")
+    collection_file_id = kingfisher_process_cursor.fetchone()[0]
+
+    kingfisher_process_cursor.execute(
+        """\
+        INSERT INTO collection_file_item (
+            number,
+            collection_file_id
+        ) VALUES (
+            0,
+            %(collection_file_id)s
+        )
+        """,
+        {"collection_file_id": collection_file_id},
+    )
+    kingfisher_process_cursor.execute("SELECT MAX(id) FROM collection_file_item")
+    collection_file_item_id = kingfisher_process_cursor.fetchone()[0]
+
+    return collection_file_item_id
+
+
+@pytest.fixture(scope="session")
+def data_rows(kingfisher_process_cursor):
+    kingfisher_process_cursor.execute(
+        """\
+        INSERT INTO data (
+            hash_md5,
+            data
+        ) VALUES (
+            'min',
+            '{"ocid": "ocds-lcuori-1", "date": "1970"}'::jsonb
+        )
+        """
+    )
+    kingfisher_process_cursor.execute("SELECT MAX(id) FROM data")
+    min_data_id = kingfisher_process_cursor.fetchone()[0]
+
+    kingfisher_process_cursor.execute(
+        """\
+        INSERT INTO data (
+            hash_md5,
+            data
+        ) VALUES (
+            'max',
+            '{"ocid": "ocds-lcuori-1", "date": "2038"}'::jsonb
+        )
+        """
+    )
+    kingfisher_process_cursor.execute("SELECT MAX(id) FROM data")
+    max_data_id = kingfisher_process_cursor.fetchone()[0]
+
+    return min_data_id, max_data_id
--- tests/dataset/test_meta_data_aggregator.py
+++ tests/dataset/test_meta_data_aggregator.py
@@ -1,4 +1,6 @@
-from dataset.metadata_aggregator import add_item, get_result
+import json
+
+from dataset.metadata_aggregator import add_item, get_kingfisher_metadata, get_result
 from pelican.util.currency_converter import bootstrap
 
 bootstrap()
@@ -12,12 +14,43 @@ items_test_compiled_releases = [
 
 
 items_test_tender_lifecycle = [
+    {"ocid": "0", "date": "2019-01-10T22:00:00+01:00", "planning": {}, "tender": {}},
     {"ocid": "0", "date": "2019-01-10T22:00:00+01:00", "tender": {}},
-    {"ocid": "0", "date": "2019-01-10T22:00:00+01:00", "tender": {}, "planning": {}},
-    {"ocid": "0", "date": "2019-01-10T22:00:00+01:00", "contracts": [{}, {}, {}]},
     {"ocid": "0", "date": "2019-01-10T22:00:00+01:00", "awards": [{}, {}, {}]},
+    {"ocid": "0", "date": "2019-01-10T22:00:00+01:00", "contracts": [{}, {}, {}]},
     {"ocid": "0", "date": "2019-01-10T22:00:00+01:00", "contracts": [{"implementation": {}}]},
 ]
+
+
+def create_compiled_release(cursor, collection_id, collection_file_item_id, data_rows):
+    min_data_id, max_data_id = data_rows
+
+    cursor.execute(
+        """\
+        INSERT INTO compiled_release (
+            ocid,
+            collection_id,
+            collection_file_item_id,
+            data_id
+        ) VALUES (
+            'ocds-lcuori-1',
+            %(collection_id)s,
+            %(collection_file_item_id)s,
+            %(max_data_id)s
+        ), (
+            'ocds-lcuori-1',
+            %(collection_id)s,
+            %(collection_file_item_id)s,
+            %(min_data_id)s
+        )
+        """,
+        {
+            "collection_id": collection_id,
+            "collection_file_item_id": collection_file_item_id,
+            "min_data_id": min_data_id,
+            "max_data_id": max_data_id,
+        },
+    )
 
 
 def test_compiled_releases():
@@ -38,3 +71,184 @@ def test_tender_lifecycle():
 
     assert result["compiled_releases"]["total_unique_ocids"] == 1
     assert result["tender_lifecycle"] == {"planning": 1, "tender": 2, "award": 3, "contract": 4, "implementation": 1}
+
+
+def test_get_kingfisher_metadata_nonexistent(kingfisher_process_cursor, caplog):
+    assert get_kingfisher_metadata(kingfisher_process_cursor, 999999999) == {
+        "collection_metadata": {
+            "data_license": None,
+            "extensions": [],
+            "ocid_prefix": None,
+            "publication_policy": None,
+            "published_from": None,
+            "published_to": None,
+            "publisher": None,
+        },
+        "kingfisher_metadata": {},
+    }
+
+    assert len(caplog.records) == 1
+    assert caplog.records[0].levelname == "WARNING"
+    assert caplog.records[0].message == "No rows found in `collection` where id = 999999999"
+
+
+def test_get_kingfisher_metadata_tree(kingfisher_process_cursor, collection_rows, caplog):
+    original, compiled = collection_rows
+
+    assert get_kingfisher_metadata(kingfisher_process_cursor, compiled) == {
+        "collection_metadata": {
+            "data_license": None,
+            "extensions": [],
+            "ocid_prefix": None,
+            "publication_policy": None,
+            "published_from": None,
+            "published_to": None,
+            "publisher": None,
+        },
+        "kingfisher_metadata": {
+            "collection_id": compiled,
+            "processing_end": "2001-02-03 07.08.09",
+            "processing_start": "2001-02-03 04.05.06",
+        },
+    }
+
+    assert len(caplog.records) == 1
+    assert caplog.records[-1].levelname == "WARNING"
+    assert caplog.records[-1].message == (f"No rows found in `release` or `record` where collection_id = {original}")
+
+
+def test_get_kingfisher_metadata_compiled_release(
+    kingfisher_process_cursor, collection_rows, collection_file_item, data_rows, caplog
+):
+    original, compiled = collection_rows
+
+    create_compiled_release(kingfisher_process_cursor, compiled, collection_file_item, data_rows)
+
+    assert get_kingfisher_metadata(kingfisher_process_cursor, compiled) == {
+        "collection_metadata": {
+            "data_license": None,
+            "extensions": [],
+            "ocid_prefix": None,
+            "publication_policy": None,
+            "published_from": "1970-01-01 00.00.00",
+            "published_to": "2038-01-01 00.00.00",
+            "publisher": None,
+        },
+        "kingfisher_metadata": {
+            "collection_id": compiled,
+            "processing_end": "2001-02-03 07.08.09",
+            "processing_start": "2001-02-03 04.05.06",
+        },
+    }
+
+    assert len(caplog.records) == 1
+    assert caplog.records[-1].levelname == "WARNING"
+    assert caplog.records[-1].message == (f"No rows found in `release` or `record` where collection_id = {original}")
+
+
+def test_get_kingfisher_metadata_release(
+    kingfisher_process_cursor, collection_rows, collection_file_item, data_rows, caplog
+):
+    original, compiled = collection_rows
+
+    create_compiled_release(kingfisher_process_cursor, compiled, collection_file_item, data_rows)
+
+    kingfisher_process_cursor.execute(
+        """\
+        INSERT INTO data (
+            hash_md5,
+            data
+        ) VALUES (
+            'empty',
+            '{}'::jsonb
+        )
+        """
+    )
+    kingfisher_process_cursor.execute("SELECT MAX(id) FROM data")
+    data_id = kingfisher_process_cursor.fetchone()[0]
+
+    kingfisher_process_cursor.execute(
+        """\
+        INSERT INTO package_data (
+            hash_md5,
+            data
+        ) VALUES (
+            'package',
+            %(data)s::jsonb
+        )
+        """,
+        {
+            "data": json.dumps(
+                {
+                    "publisher": {"name": "Acme Inc."},
+                    "license": "https://creativecommons.org/licenses/by/4.0/",
+                    "publicationPolicy": "https://example.com/policy",
+                    "extensions": [
+                        "https://raw.githubusercontent.com/open-contracting-extensions/ocds_process_title_extension/master/extension.json",  # noqa: E501
+                    ],
+                }
+            )
+        },
+    )
+    kingfisher_process_cursor.execute("SELECT MAX(id) FROM package_data")
+    package_data_id = kingfisher_process_cursor.fetchone()[0]
+
+    kingfisher_process_cursor.execute(
+        """\
+        INSERT INTO release (
+            release_id,
+            ocid,
+            collection_id,
+            collection_file_item_id,
+            data_id,
+            package_data_id
+        ) VALUES (
+            '1',
+            'ocds-lcuori-1',
+            %(collection_id)s,
+            %(collection_file_item_id)s,
+            %(data_id)s,
+            %(package_data_id)s
+        )
+        """,
+        {
+            "collection_id": original,
+            "collection_file_item_id": collection_file_item,
+            "data_id": data_id,
+            "package_data_id": package_data_id,
+        },
+    )
+
+    assert get_kingfisher_metadata(kingfisher_process_cursor, compiled) == {
+        "collection_metadata": {
+            "data_license": "https://creativecommons.org/licenses/by/4.0/",
+            "extensions": [
+                {
+                    "compatibility": ["1.1"],
+                    "contactPoint": {
+                        "email": "data@open-contracting.org",
+                        "name": "Open Contracting Partnership",
+                    },
+                    "description": {
+                        "en": "For providing overall process titles and descriptions, often to give a summary of the contracting process as a whole.",  # noqa: E501
+                    },
+                    "documentationUrl": {"en": "https://extensions.open-contracting.org/en/extensions/process_title/"},
+                    "name": {"en": "Process level title and description"},
+                    "repositoryUrl": "https://raw.githubusercontent.com/open-contracting-extensions/ocds_process_title_extension/master/extension.json",  # noqa: E501
+                    "schemas": ["release-schema.json"],
+                },
+            ],
+            "ocid_prefix": "ocds-lcuori",
+            "publication_policy": "https://example.com/policy",
+            "published_from": "1970-01-01 00.00.00",
+            "published_to": "2038-01-01 00.00.00",
+            "publisher": "Acme Inc.",
+        },
+        "kingfisher_metadata": {
+            "collection_id": compiled,
+            "processing_end": "2001-02-03 07.08.09",
+            "processing_start": "2001-02-03 04.05.06",
+        },
+    }
+
+    assert len(caplog.records) == 0
